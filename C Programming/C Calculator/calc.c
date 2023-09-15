18fbe56463ad580c3661cf791706b4a9c0009a38
skaarfungandr <mac2x.manos@gmail.com>
2023-09-15 21:02:22 +0800

    Fixed bugs

--- C Programming/C Calculator/calc.c
+++ C Programming/C Calculator/calc.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+int main()
 {
 	int nums1;
 	int nums2;
@@ -8,47 +8,44 @@ int main(int argc, char const *argv[])
 	int instances;
 	printf("Input the number of instances: \n");
 	scanf("%d", &instances);
-	for (int instances; instances > 0; --instances)
+	while (instances > 0)
 	{
+		--instances;
 		char operation;
 		printf("Select an operation: \n");
 		scanf("%c", &operation);
-		if (operation == 'a')
-		{
-			printf("Input two numbers: \n");
-			scanf("%d", &nums1);
-			scanf("%d", &nums2);
-			result = nums1 + nums2;
-			printf("The sum is %d\n", result);
-		}
-		if (operation == 's')
-		{
-			printf("Input two numbers: \n");
-			scanf("%d", &nums1);
-			scanf("%d", &nums2);
-			result = nums1 - nums2;
-			printf("The difference is %d\n", result);
-		}
-		if (operation == 'm')
-		{
-			printf("Input two numbers: \n");
-			scanf("%d", &nums1);
-			scanf("%d", &nums2);
-			result = nums1 * nums2;
-			printf("The product is %d\n", result);
-		}
-		if (operation == 'd')
-		{
-			printf("Input two numbers: \n");
-			scanf("%d", &nums1);
-			scanf("%d", &nums2);
-			result = nums1 / nums2;
-			printf("The quotient is %d\n", result);
-		}
-		else {
-			printf("Invalid operation!!\n");
-			break;
-		}
+		switch (operation) {
+			case ('+'):
+				printf("Input two numbers: ");
+				scanf("%d", &nums1);
+				scanf("%d", &nums2);
+				result = nums1 + nums2;
+				printf("The sum is %d", result);
+				break;
+			case ('-'):
+				printf("Input two numbers: ");
+				scanf("%d", &nums1);
+				scanf("%d", &nums2);
+				result = nums1 - nums2;
+				printf("The difference is %d", result);
+				break;
+			case ('*'):
+				printf("Input two numbers: ");
+				scanf("%d", &nums1);
+				scanf("%d", &nums2);
+				result = nums1 * nums2;
+				printf("The product is %d", result);
+				break;
+			case ('/'):
+				printf("Input two numbers: ");
+				scanf("%d", &nums1);
+				scanf("%d", &nums2);
+				result = nums1 / nums2;
+				printf("The quotient is %d", result);
+				break;
+			default:
+				break;
+			}
 	}
 	return 0;
 }
