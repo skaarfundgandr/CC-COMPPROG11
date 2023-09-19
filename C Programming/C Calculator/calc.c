2a96d3367417a7206b9af4ec68d74b7e33193b60
skaarfungandr <mac2x.manos@gmail.com>
2023-09-19 18:16:18 +0800

    Fixed bugs

--- C Programming/C Calculator/calc.c
+++ C Programming/C Calculator/calc.c
@@ -6,42 +6,42 @@ int main()
 	int nums2;
 	int result;
 	int instances;
+	char operation;
 	printf("Input the number of instances: \n");
 	scanf("%d", &instances);
 	while (instances > 0)
 	{
 		--instances;
-		char operation;
 		printf("Select an operation: \n");
 		scanf("%c", &operation);
 		switch (operation) {
 			case ('+'):
-				printf("Input two numbers: ");
+				printf("Input two numbers: \n");
 				scanf("%d", &nums1);
 				scanf("%d", &nums2);
 				result = nums1 + nums2;
-				printf("The sum is %d", result);
+				printf("The sum is %d\n", result);
 				break;
 			case ('-'):
-				printf("Input two numbers: ");
+				printf("Input two numbers: \n");
 				scanf("%d", &nums1);
 				scanf("%d", &nums2);
 				result = nums1 - nums2;
-				printf("The difference is %d", result);
+				printf("The difference is %d\n", result);
 				break;
 			case ('*'):
-				printf("Input two numbers: ");
+				printf("Input two numbers: \n");
 				scanf("%d", &nums1);
 				scanf("%d", &nums2);
 				result = nums1 * nums2;
-				printf("The product is %d", result);
+				printf("The product is %d\n", result);
 				break;
 			case ('/'):
-				printf("Input two numbers: ");
+				printf("Input two numbers: \n");
 				scanf("%d", &nums1);
 				scanf("%d", &nums2);
 				result = nums1 / nums2;
-				printf("The quotient is %d", result);
+				printf("The quotient is %d\n", result);
 				break;
 			default:
 				break;
