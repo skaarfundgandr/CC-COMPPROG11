8df2fa275eaea6320caab26ee3b97e83ba69b18f
skaarfungandr <mac2x.manos@gmail.com>
2023-09-21 17:57:39 +0800

    Added seperate directory for C++ Programming

---
+++ C++ Programming/C++ Calculator/calc.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int nums1, nums2, result, instances;
+	char operation;
+	cout << "Input the number of instances" << endl;
+	cin >> instances;
+	while (instances > 0) {
+		--instances;
+		cout << "input an operation" << endl;
+		cin >> operation;
+		switch (operation) {
+			case ('+'):
+				cout << "Input two numbers" << endl;
+				cin >> nums1 >> nums2;
+				result = nums1 + nums2;
+				cout << "The sum is " << result << endl;
+				break;
+			case ('-'):
+				cout << "Input two numbers" << endl;
+				cin >> nums1 >> nums2;
+				result = nums1 - nums2;
+				cout << "The difference is " << result << endl;
+				break;
+			case ('*'):
+				cout << "Input two numbers" << endl;
+				cin >> nums1 >> nums2;
+				result = nums1 * nums2;
+				cout << "The product is " << result << endl;
+				break;
+			case ('/'):
+				cout << "Input two numbers" << endl;
+				cin >> nums1 >> nums2;
+				result = nums1 / nums2;
+				cout << "The quotient is " << result << endl;
+				break;
+			default:
+				break;
+		}
+	}
+	return 0;
+}
