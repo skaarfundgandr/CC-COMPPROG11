ea2a5c871903df745d17192648d5e7889942bb93
skaarfungandr <mac2x.manos@gmail.com>
2023-10-02 18:42:40 +0800

    Added OpenAI Whisper script in Python

---
+++ Python Programming/OpenAI Whisper/whisper.py
@@ -0,0 +1,12 @@
+import openai
+
+print("Input the API key: ")
+openai.api_key = input()
+print("Input the file location")
+fileLocation = input()
+audio_file = open(fileLocation, "rb")
+transcript = openai.Audio.transcribe("whisper-1", audio_file)
+f = open("transcribed.txt", "w")
+
+transcript = repr(transcript)
+f.write(transcript)
