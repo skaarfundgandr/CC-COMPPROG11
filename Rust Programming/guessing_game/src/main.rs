34cf7c835a87b7e1c3006da54af20c66106e5304
Acer Nitro WSL <mac2x.manos@gmail.com>
2023-11-08 20:41:12 +0800

    Updated Files

--- Rust Programming/guessing_game/src/main.rs
+++ Rust Programming/guessing_game/src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut guess = String::new();
+
+    println!("Guess the number!");
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Unable to read input for guess!");
+
+
+    let guess: i64 = match guess.trim().parse(){
+        Ok(num) => num,
+        Err(_) => todo!(),
+    };
+
+    println!("You guessed {}.", guess);
+    let randint = rand.thread_rng().gen_range(..=100)
+    loop {
+       
+    }
 }
