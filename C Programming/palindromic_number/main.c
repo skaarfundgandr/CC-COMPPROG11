282cc682b7f3fc578e724a539c6bf0465e3c59f7
Acer Nitro WSL <mac2x.manos@gmail.com>
2023-11-13 18:39:49 +0800

    Added Palindrome Program

---
+++ C Programming/palindromic_number/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int num, digit1, digit3;
+	printf("Enter a 3 digit number:\n");
+	scanf("%d", &num);
+
+	if (num < 100 | num > 999){
+		printf("Invalid input!\n");
+	}
+
+	else{
+		digit1 = num % 10;
+		digit3 = num / 100 % 10;
+		(digit1 == digit3) ? printf("Palindromic!\n"):
+		printf("Non Palindromic!\n");
+	}
+	return 0;
+}
