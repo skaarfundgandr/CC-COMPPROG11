bf91c0767d8084eb2bbe4a3cb609cd4e2cd562e7
Acer Nitro WSL <mac2x.manos@gmail.com>
2023-11-21 23:05:57 +0800

    Added solution for quadrant problem in C

--- C Programming/Quadratic Formula/main.c
+++ C Programming/Quadratic Formula/main.c
@@ -20,17 +20,14 @@ int main()
   		root1 = -b + (sqrt(abs(discriminantFormula)) / (2.0 * a));
 		root2 = -b - (sqrt(abs(discriminantFormula)) / (2.0 * a));
   		printf("The roots are imaginary!\nThese are %.2lfi and %.2lfi\n", root1, root2);
-  	}
-
-  	else if (discriminantFormula > 0)
-	{
+  	} else if (discriminantFormula > 0) {
 		printf("There are two roots these are: \n");
 		root1 = -b + ((sqrt(discriminantFormula)) / (2.0 * a));
 		root2 = -b - ((sqrt(discriminantFormula)) / (2.0 * a));
 		printf("First root:\t%lf\nSecond root:\t%lf\n", root1, root2);
 	}
 	
-	else{
+	else {
 		printf("There is only one root: \n");
 		root = -b + ((sqrt(discriminantFormula)) / (2.0 * a));
 		printf("The root is:\t%lf\n", root);
---
+++ C Programming/quadrants/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int checkQuadrant(double angle);
+
+int main()
+{
+	double angle;
+	int quadrant;
+
+	printf("Enter the value of the angle: \n");
+	scanf("%lf", &angle);
+
+	angle = (int) angle % 360;
+
+	if (angle == 0.0){
+		printf("The line is on the positive X-axis.\n");
+	}
+	else if (angle == 90.0 || angle == 360){
+		printf("The line is on the positive Y-axis.\n");
+	}
+	else if (angle == 180.0){
+		printf("The line is on the negative X-axis.\n");
+	}
+	else if (angle == 270.0){
+		printf("The line is on the negative Y-axis.\n");
+	}
+	else{
+		quadrant = checkQuadrant(angle);
+		printf("The line is on quadrant %d\n.", quadrant);
+	}
+
+	return 0;
+}
+
+int checkQuadrant(double angle){
+	if (0.0 < angle && angle < 90.0){
+		return 1;
+	}
+	else if (90.0 < angle && angle < 180.0){
+		return 2;
+	}
+	else if (180.0 < angle && angle < 270.0){
+		return 3;
+	}
+	else if (270.0 < angle && angle < 360.0){
+		return 4;
+	}
+	else {
+		return 0;
+	}
+}
