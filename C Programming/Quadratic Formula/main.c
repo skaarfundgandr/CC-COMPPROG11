e4d06c539c965311cacb5f359a6311150277b8df
Acer Nitro WSL <mac2x.manos@gmail.com>
2023-11-22 20:30:23 +0800

    Update Readme.md

--- C Programming/Quadratic Formula/main.c
+++ C Programming/Quadratic Formula/main.c
@@ -6,35 +6,36 @@ double squareOf(double num1);
 
 int main()
 {
-	double a, b, c, root1, root2, root;
-	int discriminantFormula;
+    double a, b, c, root1, root2, root;
+    int discriminantFormula;
 
-	printf("Enter the value for a, b, and c: \n");
+    printf("Enter the value for a, b, and c: \n");
 
-	scanf("%lf %lf %lf", &a, &b, &c);
+    scanf("%lf %lf %lf", &a, &b, &c);
 
-	discriminantFormula = (squareOf(b)) - (4 * a * c);
+    discriminantFormula = squareOf(b) - (4 * a * c);
 
-	
-	if (discriminantFormula < 0) {
-  		root1 = -b + (sqrt(abs(discriminantFormula)) / (2.0 * a));
-		root2 = -b - (sqrt(abs(discriminantFormula)) / (2.0 * a));
-  		printf("The roots are imaginary!\nThese are %.2lfi and %.2lfi\n", root1, root2);
-  	} else if (discriminantFormula > 0) {
-		printf("There are two roots these are: \n");
-		root1 = -b + ((sqrt(discriminantFormula)) / (2.0 * a));
-		root2 = -b - ((sqrt(discriminantFormula)) / (2.0 * a));
-		printf("First root:\t%lf\nSecond root:\t%lf\n", root1, root2);
-	}
-	
-	else {
-		printf("There is only one root: \n");
-		root = -b + ((sqrt(discriminantFormula)) / (2.0 * a));
-		printf("The root is:\t%lf\n", root);
-	}
-	return 0;
+    
+    if (discriminantFormula < 0) {
+        root1 = -b + (sqrt(abs(discriminantFormula)) / (2.0 * a));
+        root2 = -b - (sqrt(abs(discriminantFormula)) / (2.0 * a));
+        printf("The roots are imaginary!\nThese are %.2lfi and %.2lfi\n", root1, root2);
+    } 
+    else if (a == 0.0 && b == 0.0) {
+        printf("There is no real solution!\n");
+    }
+    else if (a == 0.0) {
+        root = -c / b;
+        printf("There is only one root.\nWhich is %.2lf.\n", root);
+    }
+    else {
+        root1 = -b + (sqrt(discriminantFormula) / (2.0 * a));
+        root2 = -b - (sqrt(discriminantFormula) / (2.0 * a));
+        printf("There are two roots\nThese are %.2lf and %.2lf.\n", root1, root2);
+    }
+    return 0;
 }
 
 double squareOf(double num1){
-	return num1 * num1;
+    return num1 * num1;
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CC-COMPPROG11
+
+Code snippets and projects that I have done during my first year as a Computer Science student.
