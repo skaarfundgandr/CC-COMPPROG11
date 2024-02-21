d3863dbd43ef91e2c13ebf3cca129ab97a343c12
Acer Nitro WSL <mac2x.manos@gmail.com>
2024-02-21 08:31:33 +0800

    Added solution for AgeInSeconds in java

---
+++ Java Programming/AgeInSeconds/AgeInSeconds.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class AgeInSeconds{
+	public static void main(String[] args) {
+		int years, months, days, ageInSeconds;
+		double percentLived;
+
+		Scanner scan = new Scanner(System.in);
+		DecimalFormat percentform = new DecimalFormat("%##.##");
+
+		System.out.print("Enter your age in years: ");
+		years = scan.nextInt();
+		System.out.print("Enter your age in months: ");
+		months = scan.nextInt();
+		System.out.print("Enter your age in days: ");
+		days = scan.nextInt();
+
+		if (months > 5) {
+			days += 151;
+			months -= 5;
+			if (months % 2 == 1){
+				months -= 1;
+				days += 31;
+			}
+			days += (31 + 30) * (months / 2);
+		}
+		if (months <= 5 && months > 0) {
+			if (months % 2 == 1){
+				months -= 1;
+				days += 31;
+			}
+			days += (31 + 30) * (months / 2);
+		}
+		
+		days += 365 * years;
+		ageInSeconds = days * 24 * 60 * 60;
+ 		percentLived = (double) ageInSeconds / 2500000000.0;
+		
+		System.out.printf("Your age in seconds is: %d", ageInSeconds);
+		System.out.printf("You have lived %s of your life\n", percentform.format(percentLived));
+	}
+}
