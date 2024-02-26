5753f248cdc68214d4bbaf6556a5757cd511df86
Acer Nitro WSL <mac2x.manos@gmail.com>
2024-02-27 03:53:04 +0800

    Added code for LastChanceGas in java

---
+++ Java Programming/LastChanceGas/LastChanceGas.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class LastChanceGas{
+	public static void main(String[] args) {
+		int tankCapacity, milesPerGallon, gageReading;
+		float mileage;
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Tank capacity: ");
+		tankCapacity = scan.nextInt();
+		System.out.println("Gage reading: ");
+		gageReading = scan.nextInt();
+		System.out.println("Miles per gallon: ");
+		milesPerGallon = scan.nextInt();
+		
+		if ((float) (tankCapacity * (gageReading / 100.0)) * milesPerGallon < 200.0)
+			System.out.println("Get gas!");
+		else
+			System.out.println("Safe to proceed");
+	}
+}
