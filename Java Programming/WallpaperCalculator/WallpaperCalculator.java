5d83bca9b65616ca93b36c579ea35a5ce317ff86
Acer Nitro WSL <mac2x.manos@gmail.com>
2024-03-20 04:39:34 +0800

    Added code for WallpaperCalculator in Java

---
+++ Java Programming/WallpaperCalculator/WallpaperCalculator.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class WallpaperCalculator{
+	public static void main(String[] args) {
+		final float wallpaperWidth = 27, wallpaperLength = 162; 
+		float roomLength, roomWidth, roomHeight, openingWidth, openingLength, wallpaperArea;
+		int openings, count = 1, wallpaperAmount;
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Enter the length of the room in inches: ");
+		roomLength = scan.nextFloat();
+		System.out.print("Enter the width of the room in inches: ");
+		roomWidth = scan.nextFloat();
+		System.out.print("Enter the height of the room in inches: ");
+		roomHeight = scan.nextFloat();
+		System.out.print("Enter the number of openings in the room: ");
+		openings = scan.nextInt();
+
+		wallpaperArea = 2 * ((roomLength * roomWidth) + (roomWidth * roomHeight) + (roomLength * roomHeight));
+		wallpaperArea -= (2 * (roomLength * roomWidth)); // Subtracts the area of the floor and ceiling from the total
+
+		while (count <= openings){
+			System.out.printf("Enter length of opening %d: ", count);
+			openingLength = scan.nextFloat();
+			System.out.printf("Enter width of opening %d: ", count);
+			openingWidth = scan.nextFloat();
+
+			wallpaperArea -= (openingLength * openingWidth);
+			
+			++count;
+		}
+
+		wallpaperAmount = Math.round(wallpaperArea / (wallpaperLength * wallpaperWidth));
+
+		System.out.printf("You need %d wallpaper(s) to cover the room\n", wallpaperAmount);
+	}
+}
