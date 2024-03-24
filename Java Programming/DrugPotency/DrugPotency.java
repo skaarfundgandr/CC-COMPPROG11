df036da10a72ea8cde5e5451b2b02f038bef5c1c
Acer Nitro WSL <mac2x.manos@gmail.com>
2024-03-24 23:53:21 +0800

    Added code for DrugPotency in Java

---
+++ Java Programming/DrugPotency/DrugPotency.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class DrugPotency {
+	public static void main(String[] args) {
+		int month = 0;
+		double effectiveness = 100;
+
+		DecimalFormat decform = new DecimalFormat(".0#############"); 
+
+		while (true){
+			if (effectiveness < 50) {
+				System.out.printf("Month: %d\teffectiveness: %s DISCARDED\n", month, decform.format(effectiveness));
+				break;
+			}
+			if (effectiveness > 50)
+				System.out.printf("Month: %d\teffectiveness: %s\n", month, decform.format(effectiveness));
+			effectiveness -= (effectiveness * 0.04);
+			++month;
+		}
+	}
+}
