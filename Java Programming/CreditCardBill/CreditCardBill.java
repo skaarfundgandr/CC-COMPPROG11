181792dd15fa20b2ef7c923c4b03fb69175b7f91
Acer Nitro WSL <mac2x.manos@gmail.com>
2024-03-24 23:17:23 +0800

    Added code for CreditCardBill in java

---
+++ Java Programming/CreditCardBill/CreditCardBill.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class CreditCardBill {
+	public static void main(String[] args) {
+		double balance, monthlyPayment, monthlyInterest, totalPayments = 0;
+		int currMonth = 1;
+
+		Scanner scan = new Scanner(System.in);
+		DecimalFormat decform = new DecimalFormat(",##0.00");
+
+		System.out.println("Enter the current balance:");
+		balance = scan.nextDouble();
+		System.out.println("Enter the monthly interest:");
+		monthlyInterest = scan.nextDouble();
+		monthlyInterest /= 100; // Converts monthly interest to a decimal
+		System.out.println("Enter the monthly payment:");
+		monthlyPayment = scan.nextDouble();
+
+		while (balance > 0) {
+			balance += (balance * monthlyInterest);
+			balance -= monthlyPayment;
+			totalPayments += monthlyPayment;
+
+			System.out.printf("Month: %d\tbalance:%12s\ttotal payments:%12s\n", currMonth, decform.format(balance), decform.format(totalPayments));
+			++currMonth;
+		}
+	}
+}
