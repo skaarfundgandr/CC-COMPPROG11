6d90662c144f82344b72fd373a20beb66aa838d1
Acer Nitro WSL <mac2x.manos@gmail.com>
2024-03-26 04:45:54 +0800

    Fixed bugs on InputFilePrompt in java

--- Java Programming/InputFilePrompt/InputFilePrompt.java
+++ Java Programming/InputFilePrompt/InputFilePrompt.java
@@ -14,25 +14,27 @@ public class InputFilePrompt {
 			fileName = scan.nextLine();
 			hasSpace = false;
 
-			subStr = fileName.substring(fileName.length() - compStr.length(), fileName.length());
-
-			for (int i = 0; i < fileName.length(); ++i) {
-				if (fileName.charAt(i) == ' ') {
-					System.out.println(">>>file name must not contain spaces<<<");
-					hasSpace = true;
-					break;
+			if (fileName.length() >= compStr.length()){
+				subStr = fileName.substring(fileName.length() - compStr.length(), fileName.length());
+	
+				for (int i = 0; i < fileName.length(); ++i) {
+					if (fileName.charAt(i) == ' ') {
+						System.out.println(">>>file name must not contain spaces<<<");
+						hasSpace = true;
+						break;
+					}
 				}
+				if (subStr.equals(compStr) && fileName.length() > compStr.length() && hasSpace == false) {
+					System.out.println("Ok");
+					looped = false;
+				} 
+				if (fileName.length() == compStr.length() && subStr.equals(compStr))
+					System.out.println(">>>file name missing<<<");
+				if (subStr.equals(compStr) == false && hasSpace == false)
+					System.out.println(">>>file name must end in .dat<<<");
 			}
-			if (subStr.equals(compStr) && fileName.length() > compStr.length() && hasSpace == false) {
-				System.out.println("Ok");
-				looped = false;
-			} 
-			if (fileName.length() == compStr.length() && subStr.equals(compStr))
-				System.out.println(">>>file name missing<<<");
-			if (subStr.equals(compStr) == false && hasSpace == false)
+			else
 				System.out.println(">>>file name must end in .dat<<<");
-			
-
 		} while (looped);
 	}
 }
