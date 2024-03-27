b0dec1c6978584921a7ad2a55f1c4165c608ec2b
skaarfundgandr <76098147+skaarfundgandr@users.noreply.github.com>
2024-03-27 00:29:07 -0700

    Completed code for MatineeMovieTickets in Java\

--- Java Programming/MatineeMovieTickets/MatineeMovieTickets.java
+++ Java Programming/MatineeMovieTickets/MatineeMovieTickets.java
@@ -2,22 +2,44 @@ import java.util.Scanner;
 import java.text.DecimalFormat;
 
 public class MatineeMovieTickets{
-	public static void main(String[] args) {
-		final int adultAge = 13, matineeTime = 1700;
-		final double normalAdultPrice = 8.00, matineeAdultPrice = 5.00;
-		final double normalChildrenPrice = 4.00, matineeChildrenPrice = 2.00;
-		int time, age;
-		double price;
+  public static void main(String[] args) {
+    final int adultAge = 13, matineeTime = 1700;
+    final double normalAdultPrice = 8.00, matineeAdultPrice = 5.00;
+    final double normalChildrenPrice = 4.00, matineeChildrenPrice = 2.00;
+    int time, age;
+    double price = 0;
 
-		Scanner scan = new Scanner(System.in);
-		DecimalFormat currencyForm = new DecimalFormat("$###,###,###.0#");
+    Scanner scan = new Scanner(System.in);
+    DecimalFormat currencyForm = new DecimalFormat("$###,###,###.00");
 
-		System.out.print("Enter the current time: ");
-		time = scan.nextInt();
-		System.out.print("Enter age: ");
-		age = scan.nextInt();
+    System.out.print("Enter the current time: ");
+    time = scan.nextInt();
+    System.out.print("Enter age: ");
+    age = scan.nextInt();
 
-		
+    if (age > adultAge) {
+      if (time <= matineeTime && time >= 0)
+        price = matineeAdultPrice;
+      price = normalAdultPrice;
+    }
+    if (age <= adultAge) {
+      if (time <= matineeTime && time >= 0)
+        price = matineeChildrenPrice;
+      price = normalChildrenPrice;
 
-	}
+    }
+    if (time > 2200) {
+      if (age < adultAge) {
+        System.out.println("No tickets are available for non adults");
+        System.exit(0);
+      }
+      price = 4.00;
+    }
+    if (age < 0 || time < 0) {
+      System.out.println("Invalid inputs!");
+      System.exit(1);
+    }
+
+    System.out.printf("The ticket costs : %s\n", currencyForm.format(price));
+  }
 }
