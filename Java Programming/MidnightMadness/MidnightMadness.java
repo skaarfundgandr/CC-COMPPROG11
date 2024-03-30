25b82bfa0890bffaba73757ca0d82c7cd08f021c
jm <mac2x.manos@gmail.com>
2024-03-30 15:51:12 +0800

    Updated Code for MidnightMadness and MatineeMovieTickets in Java

--- Java Programming/MidnightMadness/MidnightMadness.java
+++ Java Programming/MidnightMadness/MidnightMadness.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
-public class MatineeMovieTickets{
+public class MidnightMadness{
   public static void main(String[] args) {
     final int adultAge = 13, matineeTime = 1700;
     final double normalAdultPrice = 8.00, matineeAdultPrice = 5.00;
@@ -10,7 +10,7 @@ public class MatineeMovieTickets{
     double price = 0;
 
     Scanner scan = new Scanner(System.in);
-    DecimalFormat currencyForm = new DecimalFormat("$###,###,###.00");
+    DecimalFormat currencyForm = new DecimalFormat("$,###.00");
 
     System.out.print("Enter the current time: ");
     time = scan.nextInt();
@@ -27,6 +27,13 @@ public class MatineeMovieTickets{
         price = matineeChildrenPrice;
       price = normalChildrenPrice;
 
+    }
+    if (time > 2200) {
+      if (age < adultAge) {
+        System.out.println("No tickets are available for non adults");
+        System.exit(0);
+      }
+      price = 4.00;
     }
     if (age < 0 || time < 0) {
       System.out.println("Invalid inputs!");
