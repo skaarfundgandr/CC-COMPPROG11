d9d4b028f24382ea94f016c955a02bd2ee6c66f5
jm <mac2x.manos@gmail.com>
2024-03-30 19:33:48 +0800

    Minor adjustments to multiple files in repo

--- Java Programming/InternetAcronyms/InternetAcronyms.java
+++ Java Programming/InternetAcronyms/InternetAcronyms.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class StringSwitcher{
+public class InternetAcronyms{
     public static void main (String[] args) {
         String phrase;
         char response;
