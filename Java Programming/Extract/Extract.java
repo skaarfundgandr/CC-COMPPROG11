ad9981810c6bab8044db7d31f994f286d70c9cd2
Acer Nitro WSL <mac2x.manos@gmail.com>
2024-04-17 23:32:05 +0800

    Added code for DocumentationPrinter in Java

---
+++ Java Programming/Extract/Extract.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Extract {
+	public static void main(String[] args) {
+		String input = new String();
+
+		Scanner scan = new Scanner(System.in);
+
+		while (scan.hasNextLine()) {
+			input = scan.nextLine().trim();
+
+			if (input.contains("//")) {
+				System.out.println(input.substring(input.indexOf("//"), input.length() - 1));
+			}
+		}
+	}
+}
