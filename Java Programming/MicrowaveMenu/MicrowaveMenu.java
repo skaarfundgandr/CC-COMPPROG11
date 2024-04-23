76becbe525fa236807712b423229191d11a393a7
Acer Nitro WSL <mac2x.manos@gmail.com>
2024-04-23 19:49:17 +0800

    Added code for MicrowaveMenu in Java

---
+++ Java Programming/MicrowaveMenu/MicrowaveMenu.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class MicrowaveMenu {
+	public static void main(String[] args) {
+		boolean hasAlpha;
+		String cookTime;
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Enter cook time-> ");
+		cookTime = scan.nextLine();
+		do{
+			hasAlpha = false;
+			for (int i = 0; i < cookTime.length(); ++i) {
+				if ((int) cookTime.charAt(i) < '0' || cookTime.charAt(i) > '9') {
+					System.out.println("Alphabet detected please enter again");
+					hasAlpha = true;
+					break;
+				}
+			}
+			if (hasAlpha) {
+				System.out.print("Enter cook time-> ");
+				cookTime = scan.nextLine();
+			} else if (cookTime.length() > 2) {
+				System.out.println(cookTime.substring(0, cookTime.length() - 2).concat(":").concat(cookTime.substring(cookTime.length() - 2)));
+			} else{
+				System.out.println("00:" + cookTime);
+			}
+		} while (hasAlpha);
+	}
+
+}
