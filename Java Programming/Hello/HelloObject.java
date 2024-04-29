f70558c93a8fe1283d8394fa64ea13bd25aca878
Acer Nitro WSL <mac2x.manos@gmail.com>
2024-04-29 12:09:25 +0800

    Added object code for Hello in Java

---
+++ Java Programming/Hello/HelloObject.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class HelloObject {
+	private String greeting;
+	private Scanner scan = new Scanner(System.in);
+	// Constructors
+	public HelloObject() { // Void constructor to use methods without parameters
+		this.greeting = new String("Hello");
+	} 
+	public HelloObject(String str){
+		this.greeting = str;
+	}
+	public HelloObject(HelloObject init){
+		this.greeting = init.greeting.concat(" From another object");
+	}
+	// Methods
+	public void greetLooped(int n) {
+		for (int i = 0; i < n; ++i)
+			System.out.println(greeting);
+	}
+	public void morningGreeting() {
+		System.out.println("Good morning World!");
+	}
+	public void eveningGreeting() {
+		System.out.println("Good evening World!");
+	}
+	public void greet() {
+		System.out.println("Enter Greeting:");
+		this.greeting = scan.nextLine();
+		System.out.println(greeting);
+	}
+}
