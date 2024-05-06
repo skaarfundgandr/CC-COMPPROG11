eae01aa45f843b064ebb6b799d01936f5f971906
Acer Nitro WSL <mac2x.manos@gmail.com>
2024-05-06 23:26:43 +0800

    Added code for Average in Java

---
+++ Java Programming/Weight/Average.java
@@ -0,0 +1,13 @@
+public class Average {
+	public static void main(String[] args) {
+        int avg;
+		int[] values = { 98,  99,  98,  99, 100, 101, 102, 100, 104, 105,
+                    105, 106, 105, 103, 104, 103, 105, 106, 107, 106,
+                    105, 105, 104, 104, 103, 102, 102, 101, 100, 102};
+        Weight june = new Weight(values);
+
+        avg = june.average();
+
+        System.out.println("average = " + avg);
+	}
+}
