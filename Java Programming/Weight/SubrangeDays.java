f5852cea728b96e39a2e17519fbaa50985ca0386
Acer Nitro WSL <mac2x.manos@gmail.com>
2024-05-06 23:27:12 +0800

    Added unfinished code for SubrangeDays in Java

---
+++ Java Programming/Weight/SubrangeDays.java
@@ -0,0 +1,13 @@
+public class SubrangeDays {
+	public static void main(String[] args) {
+        int firstHalfAvg, secondHalfAvg;
+		int[] values = { 98,  99,  98,  99, 100, 101, 102, 100, 104, 105,
+                    105, 106, 105, 103, 104, 103, 105, 106, 107, 106,
+                    105, 105, 104, 104, 103, 102, 102, 101, 100, 102};
+        Weight june = new Weight(values);
+
+        if (values.length % 2 == 1) {
+            
+        }
+	}
+}
