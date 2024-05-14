143bbc23c3d29be6c7cab4f0100e51381e8a37ca
Acer Nitro WSL <mac2x.manos@gmail.com>
2024-05-14 16:46:46 +0800

    Update SumAllArrayElements in java

--- Java Programming/SumAllArrayElements/SumAllArrayElements.java
+++ Java Programming/SumAllArrayElements/SumAllArrayElements.java
@@ -1,13 +1,15 @@
 public class SumAllArrayElements {
 	public static void main(String[] args) {
-		int [][] data = {{3, 2, 5},{1, 4, 4, 8, 12},{9, 1, 0, 2},{0, 2, 6, 3, -1, -8}};
+		// Variable declaration and definition
+		int[][] data = {{3, 2, 5},{1, 4, 4, 8, 12},{9, 1, 0, 2},{0, 2, 6, 3, -1, -8}};
 		int sum = 0;
-
-		for (int i = 0; i < data.length; ++i) {
-			for (int j = 0; j < data[i].length; ++j) {
-				sum += data[i][j];
+		// Add up all the elements in the array
+		for (int column = 0; column < data.length; ++column) {
+			for (int row = 0; row < data[column].length; ++row) {
+				sum += data[column][row];
 			}
 		}
+		// Output
 		System.out.println("Sum of all elements is " + sum);
 	}
 }
