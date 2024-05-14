1f614f59744ba4ee14d6a191f8284e13048f277b
Acer Nitro WSL <mac2x.manos@gmail.com>
2024-05-14 16:00:11 +0800

    Added documentation

--- Java Programming/MaximumMinimumElements/MaximumMinimumElements.java
+++ Java Programming/MaximumMinimumElements/MaximumMinimumElements.java
@@ -1,12 +1,13 @@
 public class MaximumMinimumElements {
     public static void main(String[] args) {
+        // Variable declarations and definitions
         int[][] data = { {3, 2, 5},
                          {1, 4, 4, 8, 13},
                          {9, 1, 0, 2},
                          {0, 2, 6, 3, -1, -8} };
         int max, min;
-        max = min = data[0][0];
-
+        max = min = data[0][0]; // Initialize max and min as the first element
+        // Iterate through each element in the array to find the maximum and minimum
         for (int row = 0; row < data.length; ++row) {
             for (int col = 0; col < data[row].length; ++col) {
                 if (data[row][col] > max)
