325aa14b03bab182f4e420c364774542574097a8
Acer Nitro WSL <mac2x.manos@gmail.com>
2024-05-14 17:32:49 +0800

    Add documentation

--- Java Programming/MicrowaveMenu/MicrowaveMenu.java
+++ Java Programming/MicrowaveMenu/MicrowaveMenu.java
@@ -2,33 +2,38 @@ import java.util.Scanner;
 
 public class MicrowaveMenu {
 	public static void main(String[] args) {
+		// Local declarations
 		boolean hasAlpha;
 		String cookTime;
-
+		// Object
 		Scanner scan = new Scanner(System.in);
-
+		// Input
 		System.out.print("Enter cook time-> ");
 		cookTime = scan.nextLine();
+		// Process
 		do{
 			hasAlpha = false;
 			for (int i = 0; i < cookTime.length(); ++i) {
+				// Checks for alphabets
 				if ((int) cookTime.charAt(i) < '0' || cookTime.charAt(i) > '9') {
 					System.out.println("Alphabet detected please enter again");
 					hasAlpha = true;
 					break;
 				}
 			}
-			if (hasAlpha) {
-				System.out.print("Enter cook time-> ");
+			if (hasAlpha) { // Prompt user again if alphabet is detected
+				System.out.print("Enter cook time-> "); 
 				cookTime = scan.nextLine();
-			} else if (cookTime.length() == 1){
+			}
+			// Output
+			else if (cookTime.length() == 1){
 				System.out.println("00:0" + cookTime);
 			} else if (cookTime.length() > 2) {
 				System.out.println(cookTime.substring(0, cookTime.length() - 2).concat(":").concat(cookTime.substring(cookTime.length() - 2)));
 			} else{
 				System.out.println("00:" + cookTime);
 			}
-		} while (hasAlpha);
+		} while (hasAlpha); // Loops again if alphabet is encountered
 	}
 
 }
