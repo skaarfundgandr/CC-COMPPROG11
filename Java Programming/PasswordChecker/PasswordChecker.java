aacee042cece4215e2b9336604c70e2bbcd4e50c
Acer Nitro WSL <mac2x.manos@gmail.com>
2024-05-14 17:53:42 +0800

    Redid logic for PasswordChecker and added documentation

--- Java Programming/PasswordChecker/PasswordChecker.java
+++ Java Programming/PasswordChecker/PasswordChecker.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 public class PasswordChecker {
 	public static void main(String[] args) {
+		// Variable declarations
 		boolean hasUpper, hasLower, hasDigit;
-
-		String password = new String();
+		// Objects
+		String password, upper, lower;
 		Scanner scan = new Scanner(System.in);
 
 		do {
@@ -12,22 +13,29 @@ public class PasswordChecker {
 
 			System.out.println("Enter your password:");
 			password = scan.nextLine();
-
+			// Checks if password length is atleast 7
 			if (password.length() >= 7) {
+				// Create two strings one for uppercase and one for lowercase
+				upper = new String(password.toUpperCase());
+				lower = new String(password.toLowerCase());
+
 				for (int i = 0; i < password.length(); ++i) {
-					if (Character.isUpperCase(password.charAt(i)))
+					// Compare if password contains an uppercase and a lowercase
+					if (password.charAt(i) == upper.charAt(i))
 						hasUpper = true;
-					if (Character.isLowerCase(password.charAt(i)))
+					if (password.charAt(i) == lower.charAt(i))
 						hasLower = true;
-					if (Character.isDigit(password.charAt(i)))
+					// Checks if password has digits by using ASCII values
+					if ((int) password.charAt(i) >= '0' && password.charAt(i) <= '9')
 						hasDigit = true;
+					// Exit loop if the password has digits, uppercase and lowercase
 					if (hasDigit && hasUpper && hasLower)
 						break;
 				}
-				if (!(hasUpper && hasLower && hasDigit))
+				if (!(hasUpper && hasLower && hasDigit)) // Prompts user again if password doesn't satisfy the conditions
 					System.out.println("That password is not acceptable\n");
 			} 
-			else
+			else // Prompts user if password is less than 7 characters long
 				System.out.println("That password is not acceptable\n");
 		} while (!(hasDigit && hasLower && hasUpper));
 
