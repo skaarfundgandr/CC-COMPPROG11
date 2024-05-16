d537072e6b78ad36e8475082172fb2294ca9637f
Acer Nitro WSL <mac2x.manos@gmail.com>
2024-05-16 10:45:42 +0800

    Update code for BetterDocumentationPrinter

--- Java Programming/BetterDocumentationPrinter/BetterDocumentationPrinter.java
+++ Java Programming/BetterDocumentationPrinter/BetterDocumentationPrinter.java
@@ -1,24 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class BetterDocumentationPrinter {
 	public static void main(String[] args) {
 		boolean documentationEncountered = false;
+		String input;
 
-		String input = new String();
+		try{
+			File fileObject = new File("Hello.java");
+			Scanner fscan = new Scanner(fileObject);
 
-		Scanner scan = new Scanner(System.in);
+			while (fscan.hasNextLine()) {
+				input = fscan.nextLine().trim();
 
-		while (scan.hasNextLine()) {
-			input = scan.nextLine().trim();
-
-			if (input.contains("//"))
-				System.out.println(input.substring(input.indexOf("//")));
-			if (input.contains("/*"))
-				documentationEncountered = true;
-			if (documentationEncountered)
-				System.out.println(input);
-			if (input.contains("*/"))
-				documentationEncountered = false;
+				if (input.contains("//"))
+					System.out.println(input.substring(input.indexOf("//")));
+				if (input.contains("/*"))
+					documentationEncountered = true;
+				if (documentationEncountered)
+					System.out.println(input);
+				if (input.contains("*/"))
+					documentationEncountered = false;
+			}
+		} catch (FileNotFoundException e){
+			System.out.println("File not found please try again");
 		}
 	}
 }
