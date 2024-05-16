9104d30cc97c0c89d41be356a0ec33baa18faaf4
Acer Nitro WSL <mac2x.manos@gmail.com>
2024-05-16 18:02:08 +0800

    Changed code for Extract

--- Java Programming/Extract/Extract.java
+++ Java Programming/Extract/Extract.java
@@ -1,17 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Extract {
 	public static void main(String[] args) {
 		String input; // Object Declaration
 
-		Scanner scan = new Scanner(System.in); // Scanner instance for input
-		// Process
-		while (scan.hasNextLine()) {
-			input = scan.nextLine().trim();
+		try{
+			File fileObject = new File("Hello.java");
+			Scanner scan = new Scanner(fileObject); // Scanner instance for input
+			// Process
+			while (scan.hasNextLine()) {
+				input = scan.nextLine().trim();
 
-			if (input.contains("//")) {
-				System.out.println(input.substring(input.indexOf("//"))); // Outputs comments in code
+				if (input.contains("//")) {
+					System.out.println(input.substring(input.indexOf("//"))); // Outputs comments in code
+				}
 			}
+		} catch (FileNotFoundException e){ // Detects if file does not exist and throws an error
+			System.out.println("File not found please try again");
 		}
 	}
 }
