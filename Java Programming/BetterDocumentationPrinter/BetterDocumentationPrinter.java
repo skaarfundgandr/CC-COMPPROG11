334f40f2d4256a9a5d7c81e670c0783376f5ac6f
Acer Nitro WSL <mac2x.manos@gmail.com>
2024-05-16 17:55:46 +0800

    Added documentation for BetterDocumentationPrinter

--- Java Programming/BetterDocumentationPrinter/BetterDocumentationPrinter.java
+++ Java Programming/BetterDocumentationPrinter/BetterDocumentationPrinter.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class BetterDocumentationPrinter {
 	public static void main(String[] args) {
-		boolean documentationEncountered = false;
-		String input;
+		boolean documentationEncountered = false; // Variable declaration
+		String input; // Object declaration
 
 		try{
-			File fileObject = new File("Hello.java");
-			Scanner fscan = new Scanner(fileObject);
-
+			File fileObject = new File("Hello.java"); // Create new file object
+			Scanner fscan = new Scanner(fileObject); // Scanner instance to read contents in file
+			// Driver code
 			while (fscan.hasNextLine()) {
 				input = fscan.nextLine().trim();
-
+				// Detects if input contains a documentation block or line
 				if (input.contains("//"))
-					System.out.println(input.substring(input.indexOf("//")));
+					System.out.println(input.substring(input.indexOf("//"))); // Outputs line
 				if (input.contains("/*"))
 					documentationEncountered = true;
 				if (documentationEncountered)
@@ -23,7 +23,7 @@ public class BetterDocumentationPrinter {
 				if (input.contains("*/"))
 					documentationEncountered = false;
 			}
-		} catch (FileNotFoundException e){
+		} catch (FileNotFoundException e){ // Detects if file does not exist and throws an error
 			System.out.println("File not found please try again");
 		}
 	}
